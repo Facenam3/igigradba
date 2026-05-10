5325161e768cd98c92b168dad76c13e9e5499802
Dalibor Jovanovski <dalibor.jovanov2k1@gmail.com>
2026-05-10 22:23:58 +0200

    added services page

--- igi-gradba/src/components/UI/CardSection.jsx
+++ igi-gradba/src/components/UI/CardSection.jsx
@@ -1,6 +1,12 @@
-export default function CardSection({children}){
+export default function CardSection({ children, columns = 4 }) {
     return (
-        <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-4 px-5 my-5">
+        <div
+            className={`
+                grid grid-cols-1 gap-6 px-5 my-5
+                md:grid-cols-2
+                ${columns === 3 ? "lg:grid-cols-3" : "lg:grid-cols-4"}
+            `}
+        >
             {children}
         </div>
     );
