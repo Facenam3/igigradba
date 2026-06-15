35f23b3304f3cb823f2f25344f9d3e39be38275f
Dalibor Jovanovski <dalibor.jovanov2k1@gmail.com>
2026-06-15 22:14:49 +0200

    update - auto scroll to top on switching navigation

--- igi-gradba/src/pages/layout/Root.jsx
+++ igi-gradba/src/pages/layout/Root.jsx
@@ -2,10 +2,12 @@ import { Outlet } from "react-router-dom";
 
 import MainNavigation from "../../components/navigation/MainNavigation.jsx";
 import Footer from "./Footer.jsx";
+import ScrollToTop from "../../components/UI/ScrollToTop.jsx";
 
 export default function RootLayout() {
     return (
         <div className="min-h-screen">
+            <ScrollToTop />
             <MainNavigation />
             <Outlet/>
             <Footer />
