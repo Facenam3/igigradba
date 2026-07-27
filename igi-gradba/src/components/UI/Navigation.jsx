3d0e895b4c899b99049c2c7467e27a94dacf21d1
Dalibor Jovanovski <dalibor.jovanov2k1@gmail.com>
2026-07-27 22:42:47 +0200

    updated navigation component

--- igi-gradba/src/components/UI/Navigation.jsx
+++ igi-gradba/src/components/UI/Navigation.jsx
@@ -2,22 +2,31 @@ import { NavLink } from "react-router-dom";
 import { categories } from "../../helper/categories.js";
 import ContactUs from "./buttons/ContactUs.jsx";
 
-export default function Navigation() {
+export default function Navigation({ isTransparent }) {
     return (
         <nav>
             <ul className="hidden lg:flex gap-3 items-center text-xl font-semibold capitalize  ">
                 {categories?.map((cat) => (
                     <li key={cat.id}>
                         <NavLink
-                            to={`/${cat.path}`}
-                            className={({ isActive }) =>
-                                isActive
-                                    ? "underline underline-offset-4 text-orange-500"
-                                    : "hover:text-orange-500"
+                    to={`/${cat.path}`}
+                    className={({ isActive }) => {
+                        if (isActive) {
+                            return "underline underline-offset-4 text-orange-500";
+                        }
+
+                        return `
+                            transition-colors duration-300
+                            ${
+                                isTransparent
+                                    ? "text-white hover:text-orange-400"
+                                    : "text-black hover:text-orange-500"
                             }
-                        >
-                            {cat.name}
-                        </NavLink>
+                        `;
+                    }}
+                >
+                    {cat.name}
+                </NavLink>
                     </li>
                 ))}
                 <ContactUs />
