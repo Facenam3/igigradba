e8160e0e0b196d615a5bc4a5798cd1286e5601fc
Dalibor Jovanovski <dalibor.jovanov2k1@gmail.com>
2026-09-01 08:54:23 +0200

    update: added images for project 9

---
+++ igi-gradba/src/helper/projectImages/project9Images.js
@@ -0,0 +1,32 @@
+import after from "../../assets/projects/project-9/after.JPG";
+import before from "../../assets/projects/project-9/before.JPG";
+import cover from "../../assets/projects/project-9/cover.JPG";
+import detail1 from "../../assets/projects/project-9/image-1.JPG";
+import detail2 from "../../assets/projects/project-9/image-2.JPG";
+import detail3 from "../../assets/projects/project-9/image-3.JPG";
+import detail4 from "../../assets/projects/project-9/image-4.JPG";
+import detail5 from "../../assets/projects/project-9/image-5.JPG";
+import detail6 from "../../assets/projects/project-9/image-6.JPG";
+import detail7 from "../../assets/projects/project-9/image-7.JPG";
+import detail8 from "../../assets/projects/project-9/image-8.JPG";
+import detail9 from "../../assets/projects/project-9/image-9.JPG";
+
+export const project9Images = {
+    after,
+    before,
+    cover,
+    gallery: [
+        before,
+        detail1,
+        detail2,
+        detail3,
+        detail4,
+        detail5,
+        detail6,
+        detail7,
+        detail8,
+        detail9,
+        after,
+        cover,
+    ],
+};
