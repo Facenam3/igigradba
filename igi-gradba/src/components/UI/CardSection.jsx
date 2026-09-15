bc88c5f0ce2fefd3e610f2fd0a2307bc6d4da7f4
Dalibor Jovanovski <dalibor.jovanov2k1@gmail.com>
2026-09-15 16:34:59 +0200

    fix: updated CardSection and WorkProcessCard components

--- igi-gradba/src/components/UI/CardSection.jsx
+++ igi-gradba/src/components/UI/CardSection.jsx
@@ -2,7 +2,7 @@ export default function CardSection({ children, columns = 4 }) {
     return (
         <div
             className={`
-                grid grid-cols-1 gap-6 px-5 my-5
+                grid grid-cols-1 gap-6 px-5 mt-8
                 md:grid-cols-2
                 ${columns === 3 ? "lg:grid-cols-3" : "lg:grid-cols-4"}
             `}
